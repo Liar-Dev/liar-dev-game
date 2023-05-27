77e3925ed84bab257079b6f351adeab5b2d3e6e8
gosekose <ksyn1611@naver.com>
2023-05-27 12:42:36 +0900

    Test: 배포 테스트

--- src/main/java/liar/gamemvcservice/game/repository/TopicRepository.java
+++ src/main/java/liar/gamemvcservice/game/repository/TopicRepository.java
@@ -2,9 +2,18 @@ package liar.gamemvcservice.game.repository;
 
 import liar.gamemvcservice.game.domain.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     Topic findTopicById(Long id);
+
+    @Query("select t from Topic t where t.id in :ids")
+    List<Topic> findTopicsByIds(@Param("ids") List<Long> ids);
+
+    List<Topic> findByIdIn(List<Long> ids);
 }
